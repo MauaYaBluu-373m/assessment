b72f7aef3b7b9baf6f07f89f4188a41836384d8d
MauaYaBluu-373m <37008979+MauaYaBluu-373m@users.noreply.github.com>
2018-03-03 14:35:11 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -9,29 +9,30 @@
      * 指定した要素の子供を全て削除する
      * @param {HTMLElement} element HTMLの要素
      */
+
     function removeAllChildren(element) {
         while (element.firstChild) {
             element.removeChild(element.firstChild);
         }
     }
-
-    // 診断結果表示エリアの作成、ボタンが押されてから表示されるように。
-    removeAllChildren(resultDivided);     //同じ結果が表示されないように。
-
-    assessmentButton.onclick = function () {
+    
+    assessmentButton.onclick = () => {
         const userName = userNameInput.value;
         if (userName.length === 0) { //名前がからの時は処理を終了する。ガード区という。
             return;
         }
 
-    // whileはループの一種で、（）内が真である限り{}する。
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivided.appendChild(header);
-    const paragraph = document.createElement('p');
-    const result = assessment(userName);
-    paragraph.innerText = result;
-    resultDivided.appendChild(paragraph);
+        // 診断結果表示エリアの作成、ボタンが押されてから表示されるように。
+        removeAllChildren(resultDivided);     //同じ結果が表示されないように。
+        // whileはループの一種で、（）内が真である限り{}する。
+        const header = document.createElement('h3');
+        header.innerText = '診断結果';
+        resultDivided.appendChild(header);
+        
+        const paragraph = document.createElement('p');
+        const result = assessment(userName);
+        paragraph.innerText = result;
+        resultDivided.appendChild(paragraph);
     
     /*
     //TODO ツイートエリアの作成  ⬇元のURL
@@ -50,7 +51,7 @@
     tweetDivided.appendChild(anchor);
 
     twttr.widgets.load();
-    };
+};
 
     userNameInput.onkeydown = (event) => {
         if (event.keyCode === 13) {       //13番はエンターキー
@@ -61,22 +62,22 @@
 
     
     const answers = [
-        '{userName}さんのいいところは声です。{userName}さんの特徴的な声はみなを惹きつけ、心に残ります。',
-        '{userName}さんのいいところはまなざしです。{userName}さんに見つめられた人は、気になって仕方がないでしょう。',
-        '{userName}さんのいいところは情熱です。{userName}さんの情熱に周りの人は感化され、前向きになれます。',
-        '{userName}さんのいいところは厳しさです。{userName}さんの厳しさがものごとをいつも成功に導きます。',
-        '{userName}さんのいいところは知識です。博識な{userName}さんを多くの人が頼りにしています。',
-        '{userName}さんのいいところはユニークさです。{userName}さんだけのその特徴が皆を楽しくさせます。',
-        '{userName}さんのいいところは用心深さです。{userName}さんの洞察に、多くの人が助けられます。',
-        '{userName}さんのいいところは見た目です。内側から溢れ出る{userName}さんの良さに皆が気を惹かれます。',
-        '{userName}さんのいいところは決断力です。{userName}さんがする決断にいつも助けられる人がいます。',
-        '{userName}さんのいいところは思いやりです。{userName}さんに気をかけてもらった多くの人が感謝しています。',
-        '{userName}さんのいいところは感受性です。{userName}さんが感じたことに皆が共感し、わかりあうことができます。',
-        '{userName}さんのいいところは節度です。強引すぎない{userName}さんの考えに皆が感謝しています。',
-        '{userName}さんのいいところは好奇心です。新しいことに向かっていく{userName}さんの心構えが多くの人に魅力的に映ります。',
-        '{userName}さんのいいところは気配りです。{userName}さんの配慮が多くの人を救っています。',
-        '{userName}さんのいいところはその全てです。ありのままの{userName}さん自身がいいところなのです。',
-        '{userName}さんのいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}さんが皆から評価されています。'
+        '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
+        '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
+        '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化され、前向きになれます。',
+        '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
+        '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
+        '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
+        '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
+        '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
+        '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
+        '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
+        '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
+        '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
+        '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
+        '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
+        '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
+        '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
     ];
 
     /**
@@ -94,12 +95,12 @@
         // 文字のコード番号の合計を回答の配列の数で割って添字の数値を求める
         const index = sumOfcharCode % answers.length;
         let result = answers[index];
+        
         result = result.replace(/\{userName\}/g, userName); //正規表現。gをつけることで全ての{userName}が書き換えられる
-
-
         // TODO 診断処理を実装する
         return result;
     }
+    
     // 正しく動作しているかテストする
     console.assert(
         assessment('太郎') === assessment('太郎'),
